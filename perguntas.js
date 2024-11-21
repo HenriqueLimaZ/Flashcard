bcda40a57d7f26ff9df1f3d7e0f5826bfe88133b
HenriqueLimaZ <00001106743684sp@al.educacao.sp.gov.br>
2024-11-21 08:49:23 -0300

    add perguntas e alterando

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,53 @@
 criaCartao(
-    'Piadas',
-    'O queacontece qundo chove na Inglaterra?',
+    'Piadas Ruins',
+    'O que acontece qundo chove na Inglaterra?',
     'Vira Inglalama'
 )
 
 criaCartao(
-    'Piadas',
+    'Piadas Ruins',
     'Como se chamam a pessoa que viu o Thor de perto?',
     'Vi-thor'
 )
 
 criaCartao(
-    'Piadas',
+    'Piadas Ruins',
     'Porque há uma cama elástica no Polo Norte?',
     'Para o Urso Polar'
 )
 
 criaCartao(
-    'Piadas',
+    'Piadas Ruins',
     'A plantinha foi ao hospital, mas não foi atendida. Por que?',
     'Porque só tinha médico de Plantão'
 )
 
 criaCartao(
-    'Piadas',
+    'Piadas Ruins',
     'Quando os americanos comeram carne pela primeira vez?',
     'Quando chegou Cristóvão “Com Lombo”'
 )
 
 criaCartao(
-    'Piadas',
+    'Piadas Ruins',
     'Qual é o melhor chá para a calvíce?',
     'Chá-peu'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Por que o médico está sempre calmo?',
+    'Porque ele tem muitos pacientes.'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Que tipo de carro o Yoda dirige?',
+    'Toyoda.'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Como você chama uma pilha de gatinhos?',
+    'Uma Meowntanha.'
 )
